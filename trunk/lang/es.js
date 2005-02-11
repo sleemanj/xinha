6b66011eaa68b57e0b20703cd736466095bf4960
xinha <xinha@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-02-11 12:14:44 +0000

    Bootstrap

---
+++ trunk/lang/es.js
@@ -0,0 +1,51 @@
+// I18N constants
+
+HTMLArea.I18N = {
+
+  // the following should be the filename without .js extension
+  // it will be used for automatically load plugin language.
+  lang: "es",
+
+  tooltips: {
+    bold:           "Negrita",
+    italic:         "Cursiva",
+    underline:      "Subrayado",
+    strikethrough:  "Tachado",
+    subscript:      "Sub?ndice",
+    superscript:    "Super?ndice",
+    justifyleft:    "Alinear a la Izquierda",
+    justifycenter:  "Centrar",
+    justifyright:   "Alinear a la Derecha",
+    justifyfull:    "Justificar",
+    orderedlist:    "Lista Ordenada",
+    unorderedlist:  "Lista No Ordenada",
+    outdent:        "Aumentar Sangr?a",
+    indent:         "Disminuir Sangr?a",
+    forecolor:      "Color del Texto",
+    hilitecolor:    "Color del Fondo",
+    inserthorizontalrule: "L?nea Horizontal",
+    createlink:     "Insertar Enlace",
+    insertimage:    "Insertar Imagen",
+    inserttable:    "Insertar Tabla",
+    htmlmode:       "Ver Documento en HTML",
+    popupeditor:    "Ampliar Editor",
+    about:          "Acerca del Editor",
+    showhelp:       "Ayuda",
+    textindicator:  "Estilo Actual",
+    undo:           "Deshacer",
+    redo:           "Rehacer",
+    cut:            "Cortar selecci?n",
+    copy:           "Copiar selecci?n",
+    paste:          "Pegar desde el portapapeles"
+  },
+
+  buttons: {
+    "ok":           "Aceptar",
+    "cancel":       "Cancelar"
+  },
+
+  msg: {
+    "Path":         "Ruta",
+    "TEXT_MODE":    "Esta en modo TEXTO. Use el boton [<>] para cambiar a WYSIWIG"
+  }
+};
---
+++ trunk/plugins/SuperClean/lang/en.js
@@ -0,0 +1,27 @@
+// I18N constants
+
+// LANG: "en", ENCODING: UTF-8 | ISO-8859-1
+// Author: Mihai Bazon, http://dynarch.com/mishoo
+
+// FOR TRANSLATORS:
+//
+//   1. PLEASE PUT YOUR CONTACT INFO IN THE ABOVE LINE
+//      (at least a valid email address)
+//
+//   2. PLEASE TRY TO USE UTF-8 FOR ENCODING;
+//      (if this is not possible, please include a comment
+//       that states what encoding is necessary.)
+
+SuperClean.I18N = {
+  "tooltip"                :    "Clean Up HTML",
+  "select_from"            :    "Please select from the following cleaning options...",
+  "general_tidy"           :    "General tidy up and correction of some problems.",
+  "word_clean"             :    "Clean bad HTML from Microsoft Word",
+  "faces"                  :    'Remove custom typefaces (font "styles").',
+  "sizes"                  :    'Remove custom font sizes.',
+  "colors"                 :    'Remove custom text colors.',
+  "go"                     :    'Go',
+  'cancel'                 :    'Cancel',
+  'failed'                 :    'Tidy failed.  Check your HTML for syntax errors.',
+  'nothingtodo'             :    'You don\'t have anything to tidy!'
+};
