456c265efdbd471f01c5f70ad25f9e0824c4f2db
James Sleeman <james@gogo.co.nz>
2005-03-05 05:02:30 +0000

    Adding InsertAnchor plugin, appears to work wihtout any problems.  Still need to find a better way for plugins like this to insert into the toolbar.
    Note the php file is for the original dev to use gettext through php.  
    I'll remove those files as they are specific to his install.

---
+++ plugins/InsertAnchor/lang/en.js
@@ -0,0 +1,8 @@
+// I18N constants
+
+// LANG: "en", ENCODING: UTF-8 | ISO-8859-1
+
+InsertAnchor.I18N = {
+  "IA-insert-anchor" : "Insert Anchor",
+  "IA-anchorname" : "Anchor name"
+};
---
+++ plugins/InsertAnchor/lang/vps.js
@@ -0,0 +1,3 @@
+
+//externe php-datei für sprace includen:
+document.write("<script type='text/javascript' src='" + _editor_url + "/plugins/InsertAnchor/lang/vps.php'><"+"/script>");
