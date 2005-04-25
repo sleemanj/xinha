4bef4f0ed1ad1baf77813ed198a0bb725d345048
yermol <yermol@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-04-25 03:50:03 +0000

    Fixed exceptions on closed trace window. each domain now opens it's own trace window. updated README files and index.html

--- branches/unified_backend/ddt.js
+++ branches/unified_backend/ddt.js
@@ -44,6 +44,13 @@
 // 2005-04-22 YmL:
 //	.	now uses a popup window for all messages.
 //		writing to a textarea was simply too slow.
+//
+// 2005-04-24 YmL:
+// .	popup window name now includes location.host so we can have multiple
+//		instance of Xinha running on different domains. Avoids:
+//
+// 	Error: uncaught exception: Permission denied to get property Window.DDT_STATUS
+// .	setting the title of the debug window so you can tell which domain it refers to.
 // -----------------------------
 
 /**
@@ -105,25 +112,34 @@ DDT.prototype._ddtOn = function()
 if ( this.popup == null )
 	{
 
-	// open the popup window
+	// open the popup window. 
+	//
+	// IMPORTANT: because we are likely to copy a debugging version of Xinha 
+	// onto different sites and possibly run them at the same time, each domain 
+	// instance of Xinha will need it's own popup window .. this is because one 
+	// domain cannot write to a window opened by another domain. 
+	//
+	// We'll just use the hostname here to make the window name unique.
 
-	this.popup = window.open( "", "ddt_popup",
+	this.popup = window.open( "", "ddt_popup_" + location.host,
 			      "toolbar=no,menubar=no,personalbar=no,width=800,height=450," +
 			      "scrollbars=no,resizable=yes,modal=yes,dependable=yes");
 
 	// it's possible that the window was left open or we are another object
 	// sharing the same window.
 
-	if ( typeof this.popup.document.getElementsByTagName("p")[0] == 'undefined' )
+	if ( typeof this.popup.DDT_STATUS == 'undefined' )
 		{
 
-		var content = "<html><body><p>DDT</p><p><a href=\"javascript:document.write( '<hr>');\">ADD SEPARATOR</a></p></body></html>";
+		var content = "<html><head><title>Trace Messages for '" + location.host + "'</title></head><body><p>DDT</p><p><a href=\"javascript:document.write( '<hr>');\">ADD SEPARATOR</a></p></body></html>";
 		this.popup.document.write( content );
 
 		var header = this.popup.document.createElement( "h3" );
 		var message = this.popup.document.createTextNode( "_ddt Trace Messages" );
 		header.appendChild( message );
 		this.popup.document.body.appendChild( header );
+
+		this.popup.DDT_STATUS = "ok";
 
 		}	// end of if the popup document hadn't been created.
 
@@ -174,7 +190,20 @@ DDT.prototype._ddt = function( file, line, msg )
 
 if ( this.state == "on" )
 	{
-	this.popup.document.write( "(" + this.name + ") " + file + ":" + line + " - " + msg + "<br>\n" );
+
+	// fireFox will generate an exception if the window is closed.
+
+	try
+		{
+		this.popup.document.write( "(" + this.name + ") " + file + ":" + line + " - " + msg + "<br>\n" );
+		}
+	catch( e )
+		{
+
+		// chances are the window was closed. For the moment we'll ignore this.
+		//
+		// alert( "exception on write of message '" + msg + "' e== '" + e.toString() + "'" );
+		}
 	}
 
 }
@@ -191,7 +220,7 @@ if ( this.state == "on" )
 DDT.prototype._ddtDumpObject = function( file, line, msg, obj )
 {
 
-this._ddt( "ddt.js","194", file, line, msg );
+this._ddt( "ddt.js","208", file, line, msg );
 
 for (var x in obj)
    {
