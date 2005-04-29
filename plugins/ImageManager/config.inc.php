3e624491b523228ba0fbfca18f83da89db3b1ccf
yermol <yermol@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-04-29 23:25:29 +0000

    turn off debugging trace messages

--- plugins/ImageManager/config.inc.php
+++ plugins/ImageManager/config.inc.php
@@ -249,10 +249,10 @@ define( "IM_CONFIG_LOADED", "yes" );
 include_once( "ddt.php" );
 
 // uncomment to send debug messages to a local file
-_setDebugLog( "/tmp/debug_log.txt" );
+// _setDebugLog( "/tmp/debug_log.txt" );
 
 // turn debugging on everywhere.
-_ddtOn();
+// _ddtOn();
 
 // END
 
