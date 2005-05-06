b46640525d3470aa25a11f26b0ae5105bc8d0116
James Sleeman <james@gogo.co.nz>
2005-05-06 19:47:41 +0000

    Apply #124

--- dialog.js
+++ dialog.js
@@ -19,6 +19,7 @@ function Dialog(url, action, init) {
 		init = window;	// pass this window object by default
 	}
 	Dialog._geckoOpenModal(url, action, init);
+  window.focus();
 };
 
 Dialog._parentEvent = function(ev) {
