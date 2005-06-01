024ca0e9f5ebc37ca5212ef95bc92817d69d4f02
gocher <gocher@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-06-01 14:11:09 +0000

    new plugin insert InsertMarquee

---
+++ trunk/plugins/InsertMarquee/lang/fr.js
@@ -0,0 +1,13 @@
+// I18N constants
+
+// LANG: "fr", ENCODING: UTF-8 | ISO-8859-1
+// Author: Udo Schmal
+//
+// (c) Udo Schmal 2004
+// Distributed under the same terms as HTMLArea itself.
+// This notice MUST stay intact for use (see license.txt).
+
+{
+	"Insert scrolling marquee": "Insérez marquee",
+	"Cancel": "Annulation"
+};
---
+++ trunk/plugins/InsertMarquee/lang/it.js
@@ -0,0 +1,13 @@
+// I18N constants
+
+// LANG: "it", ENCODING: UTF-8 | ISO-8859-1
+// Author: Udo Schmal
+//
+// (c) Udo Schmal
+// Distributed under the same terms as HTMLArea itself.
+// This notice MUST stay intact for use (see license.txt).
+
+{
+	"Insert scrolling marquee": "Inserisca marquee",
+	"Cancel": "Annullamento"
+};
