8f8853c0df8037dd8f079b48ce94c52b7c19f805
gocher <gocher@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-06-01 14:11:09 +0000

    new plugin insert InsertMarquee

---
+++ plugins/InsertMarquee/lang/de.js
@@ -0,0 +1,20 @@
+// I18N constants
+
+// LANG: "de", ENCODING: UTF-8 | ISO-8859-1
+// Author: Udo Schmal
+//
+// (c) Udo Schmal 2004
+// Distributed under the same terms as HTMLArea itself.
+// This notice MUST stay intact for use (see license.txt).
+
+{ "Insert scrolling marquee": "Marquee einfügen",
+	"Insert marquee": "Marquee einfügen",	
+	"Direction:": "Ausrichtung:",
+	"Behavior:": "Bewegung:",
+	"Text:": "Text:",
+	"Background-Color:": "Hintergrundfarbe:",
+	"Width:": "Breite:",
+	"Height:": "Höhe:",
+	"Speed Control": "Geschwindigkeitseinstellungen",
+	"Cancel": "Abbrechen"
+};
