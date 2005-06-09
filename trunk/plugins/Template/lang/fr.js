3f97b2d18c1cb574ef9132dca797cb1829886512
gocher <gocher@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-06-09 14:02:31 +0000

    Template plugin to be continued in the next days but it's a start!

---
+++ trunk/plugins/Template/lang/fr.js
@@ -0,0 +1,14 @@
+// I18N constants
+
+// LANG: "fr", ENCODING: UTF-8 | ISO-8859-1
+// Sponsored by http://www.systemconcept.de
+// Author: Holger Hees, <hhees@systemconcept.de>
+//
+// (c) systemconcept.de 2004
+// Distributed under the same terms as HTMLArea itself.
+// This notice MUST stay intact for use (see license.txt).
+
+{
+	"Insert template"          : "Insérez le template",
+	"Cancel"                   : "Annulation"
+};
