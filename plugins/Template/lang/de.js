fba07cfbac32ca7d63671fcdead33492bded80ae
gocher <gocher@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-06-09 14:02:31 +0000

    Template plugin to be continued in the next days but it's a start!

---
+++ plugins/Template/lang/de.js
@@ -0,0 +1,14 @@
+// I18N constants
+
+// LANG: "de", ENCODING: UTF-8 | ISO-8859-1
+// Sponsored by http://www.systemconcept.de
+// Author: Udo Schmal, <udo.schmal@t-online.de>
+//
+// (c) systemconcept.de 2004
+// Distributed under the same terms as HTMLArea itself.
+// This notice MUST stay intact for use (see license.txt).
+
+{
+	"Insert template"          : "Template einfügen",
+	"Cancel"                   : "Abbrechen"
+};
