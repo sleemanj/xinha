1adacba5235ca1e375dbaf76f585520f085ca74c
gocher <gocher@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-06-24 08:24:47 +0000

    new plugin UnFormat

---
+++ plugins/UnFormat/lang/de.js
@@ -0,0 +1,10 @@
+{
+  "Page Cleaner"      : "Seite bereinigen",
+  "Cleaning Area"     : "Reinigungsbereich",
+  "Selection"         : "Ausgewählter Bereich",
+  "All"               : "Alles",
+  "Cleaning options"  : "Reinigungsoptionen",
+  "Formatting:"       : "Formatierung:",
+  "All HTML:"         : "Ganzes HTML:",
+  "Select which types of formatting you would like to remove." : "Wählen Sie aus welche Formatierungen Sie entfernen wollen."
+};
---
+++ plugins/UnFormat/lang/nl.js
@@ -0,0 +1,10 @@
+{
+  "Page Cleaner"      : "Pagina Schoonmaker",
+  "Cleaning Area"     : "Schoonmaak gebied",
+  "Selection"         : "Geselecteerde tekst",
+  "All"               : "Alles",
+  "Cleaning options"  : "Schoonmaak opties",
+  "Formatting:"       : "Format",
+  "All HTML:"         : "Alle html",
+  "Select which types of formatting you would like to remove." : "Selecteer welke types van Formatteren je wilt verwijderen"
+};
