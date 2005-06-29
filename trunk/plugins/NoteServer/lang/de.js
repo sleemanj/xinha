56c0e25e266632fa86fbd7e39f01032f2bad9b9b
gocher <gocher@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-06-29 13:25:11 +0000

    new plugin NoteServer (GUIDO Music Notation)

---
+++ trunk/plugins/NoteServer/lang/de.js
@@ -0,0 +1,15 @@
+﻿// I18N constants
+
+// LANG: "de", ENCODING: UTF-8 | ISO-8859-1
+// Sponsored by http://www.systemconcept.de
+// Author: Holger Hees, <hhees@systemconcept.de>
+//
+// (c) systemconcept.de 2004
+// Distributed under the same terms as HTMLArea itself.
+// This notice MUST stay intact for use (see license.txt).
+
+{
+  "Insert GUIDO Music Notation" : "Einfügung einer GUIDO Musik-Tonfolge",
+  "MIDI File"                   : "MIDI Datei",
+  "Source Code"                 : "Quell-Code"
+};
---
+++ trunk/plugins/NoteServer/lang/fr.js
@@ -0,0 +1,15 @@
+// I18N constants
+
+// LANG: "fr", ENCODING: UTF-8 | ISO-8859-1
+// Sponsored by http://www.systemconcept.de
+// Author: Holger Hees, <hhees@systemconcept.de>
+//
+// (c) systemconcept.de 2004
+// Distributed under the same terms as HTMLArea itself.
+// This notice MUST stay intact for use (see license.txt).
+
+{
+  "Insert GUIDO Music Notation" : "Insertion d'une partition musicale GUIDO",
+  "MIDIFile"                    : "Fichier MIDI",
+  "SourceCode"                  : "Code source : "
+};
