6e9aed7f9b34704317b93d4610b1ee8955e1c1f8
gocher <gocher@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-07-18 18:43:48 +0000

    Ticket #392 expanding the language for this plugin, and it requries some slight modofications from Kim Steinhaug

--- plugins/NoteServer/lang/de.js
+++ plugins/NoteServer/lang/de.js
@@ -9,7 +9,16 @@
 // This notice MUST stay intact for use (see license.txt).
 
 {
-  "Insert GUIDO Music Notation" : "Einfügung einer GUIDO Musik-Tonfolge",
-  "MIDI File"                   : "MIDI Datei",
-  "Source Code"                 : "Quell-Code"
+  "Insert GUIDO Music Notation": "Einfügung einer GUIDO Musik-Tonfolge",
+  "Guido code": "Guido code",
+  "Options": "Einstellungen",
+  "Format": "Format",
+  "Image in applet": "Bild im Applet",
+  "Zoom": "Zoom",
+  "MIDI File": "MIDI Datei",
+  "Image Preview": "Bild Voransicht",
+  "Source Code": "Quell-Code",
+  "Preview": "Voransicht",
+  "Add MIDI link to allow students to hear the music": "MIDI-Link hinzufügen um Studenten das hören der Musik zu ermöglichen",
+  "Add GUIDO Code in a textbox on the page": "GUIDO Code in einer Textbox auf der Seite anzeigen"
 };
