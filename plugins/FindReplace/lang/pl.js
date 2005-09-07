5f518cf43349d869a32234cedce5ee2f18e5b11c
gocher <gocher@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-09-07 05:49:25 +0000

    Ticket #483 Added Polish translation files for a few plugins

---
+++ plugins/FindReplace/lang/pl.js
@@ -0,0 +1,38 @@
+/ I18N constants
+
+// LANG: "pl", ENCODING: UTF-8
+// translated: Krzysztof Kotowicz, koto1sa@o2.pl, http://www.eskot.krakow.pl/portfolio
+
+// FOR TRANSLATORS:
+//
+//   1. PLEASE PUT YOUR CONTACT INFO IN THE ABOVE LINE
+//      (at least a valid email address)
+//
+//   2. PLEASE TRY TO USE UTF-8 FOR ENCODING;
+//      (if this is not possible, please include a comment
+//       that states what encoding is necessary.)
+
+{
+  // messages
+  "Substitute this occurrence?":     "Zamienić to wystąpienie?",
+  "Enter the text you want to find": "Podaj tekst, jaki chcesz wyszukać",
+  "Inform a replacement word":       "Podaj tekst do zamiany",
+  "found items":                     "znalezionych",
+  "replaced items":                  "zamienionych",
+  "found item":                      "znaleziony",
+  "replaced item":                   "zamieniony",
+  "not found":                       "nie znaleziony",
+  // window
+  "Find and Replace":                "Znajdź i zamień",
+  "Search for:":                     "Szukaj:",
+  "Replace with:":                   "Zamień na:",
+  "Options":                         "Opcje", 
+  "Whole words only":                "Całe słowa",
+  "Case sensitive search":           "Wg wielkości liter",
+  "Substitute all occurrences":      "Zamień wszystkie wystąpienia",
+  "Clear":                           "Wyczyść",
+  "Highlight":                       "Podświetl",
+  "Undo":                            "Cofnij",
+  "Next":                            "Następny",
+  "Done":                            "Gotowe"
+}
---
+++ plugins/FullPage/lang/pl.js
@@ -0,0 +1,29 @@
+// I18N for the FullPage plugin
+
+// LANG: "pl", ENCODING: UTF-8
+// translated: Krzysztof Kotowicz, koto1sa@o2.pl, http://www.eskot.krakow.pl/portfolio
+
+// FOR TRANSLATORS:
+//
+//   1. PLEASE PUT YOUR CONTACT INFO IN THE ABOVE LINE
+//      (at least a valid email address)
+//
+//   2. PLEASE TRY TO USE UTF-8 FOR ENCODING;
+//      (if this is not possible, please include a comment
+//       that states what encoding is necessary.)
+
+{
+  "Alternate style-sheet:": "Alternatywny arkusz stylów:",
+  "Background color:": "Kolor tła:",
+  "Cancel": "Anuluj",
+  "DOCTYPE:": "DOCTYPE:",
+  "Document properties": "Właściwości dokumentu",
+  "Document title:": "Tytuł dokumentu:",
+  "OK": "OK",
+  "Primary style-sheet:": "Arkusz stylów:",
+  "Text color:": "Kolor tekstu:",
+  "Character set:": "Zestaw znaków",
+  "Description:": "Opis",
+  "Keywords:": "Słowa kluczowe",
+  "UTF-8 (recommended)": "UTF-8 (zalecany)"
+}
