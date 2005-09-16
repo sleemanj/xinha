c42ab844fc6df72d2675909767c626e51808efac
gocher <gocher@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-09-16 11:58:18 +0000

    New plugin BackgroundImage

---
+++ plugins/BackgroundImage/lang/de.js
@@ -0,0 +1,14 @@
+// I18N constants
+// LANG: "de", ENCODING: UTF-8
+// Author: Udo Schmal, <schmal@schaffrath-neuemedien.de>
+//
+// (c) Udo Schmal & Schaffrath NeueMedien 2005
+// Distributed under the same terms as HTMLArea itself.
+// This notice MUST stay intact for use (see license.txt).
+
+{
+	"Set page background image": "Seiten-Hintergrundbild setzen",
+  "Set Page Background Image": "Seiten-Hintergrundbild setzen",
+  "Remove Current Background": "Aktuellen Hintergrund entfernen",
+  "Cancel": "Abbrechen"
+};
