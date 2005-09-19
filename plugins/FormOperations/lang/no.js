a41bcad87fb55e36224471dc2eccd0329ab87856
gocher <gocher@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-09-19 19:13:24 +0000

    Ticket #502, #504, #505, #506 NO langfiles for FormOperations, InsertPagebreak, BackgroundImage, InsertPicture

---
+++ plugins/FormOperations/lang/no.js
@@ -0,0 +1,18 @@
+// I18N constants
+// LANG: "no", ENCODING: UTF-8
+// Author: Udo Schmal, <schmal@schaffrath-neuemedien.de>
+// translated: Kim Steinhaug, http://www.steinhaug.com/, kim@steinhaug.com
+// 
+// (c) Udo Schmal & Schaffrath NeueMedien 2005
+// Distributed under the same terms as HTMLArea itself.
+// This notice MUST stay intact for use (see license.txt).
+
+{
+  "Insert a Form.": "Sett inn skjema",
+  "Insert a text, password or hidden field.": "Sett inn formfelt",
+  "Insert a multi-line text field.": "Sett inn tekstfelt med flere linjer",
+  "Insert a select field.": "Sett inn valgboks/ netrekksboks",
+  "Insert a check box.": "Hakeboks",
+  "Insert a radio button.": "Sett inn en radioknapp",
+  "Insert a submit/reset button.": "Sett inn send-/nullstill knapp for skjemaet"
+};
---
+++ plugins/InsertPicture/lang/no.js
@@ -0,0 +1,29 @@
+// LANG: "de", ENCODING: UTF-8 | ISO-8859-1
+// Sponsored by http://www.schaffrath-neuemedien.de
+// Author: Udo Schmal (gocher), <udo.schmal@t-online.de>
+// translated: Kim Steinhaug, http://www.steinhaug.com/, kim@steinhaug.com
+//
+// (c) Udo Schmal & Schaffrath NeueMedien 2005
+// Distributed under the same terms as HTMLArea itself.
+// This notice MUST stay intact for use (see license.txt).
+
+{
+  "Insert Image": "Sett inn bilde",
+  "Image Preview:": "Forhåndsvisning:",
+  "Image URL:": "Bildets URL:",
+  "Preview": "Forhåndsvisning",
+  "Alternate text:": "Alternativ tekst",  
+  "Layout": "Oppsett",
+  "Alignment:": "Plassering",
+  "Border thickness:": "Ramme tykkelse:",
+  "Spacing": "Luft rundt bildet",
+  "Horizontal:": "Horisontal:",
+  "Vertical:": "Vertikal:",
+  "The file you are uploading doesn't have the correct extension.": "Bildet du laster opp har et ugyldig format, opplastning avbrutt",
+  "The file you are uploading already exists.": "Bildet du prøver å laste opp eksisterer allerede på serveren",
+  "The file you are uploading is to big. The max Filesize is": "Bildet du laster opp er for stort, maks tillatt størrelse er",
+  "Images on the Server:": "Bilder på serveren:",
+  "Please select a file to upload.": "Velg bilde for opplastning til server",
+  "Upload file": "Last opp bilde",
+  "Open file in new window": "Åpne bilde i nytt vindu"
+};
