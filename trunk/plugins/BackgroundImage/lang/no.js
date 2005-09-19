e360fb7d2fcb808b1c881881ad88a5d940b0a0ee
gocher <gocher@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-09-19 19:13:24 +0000

    Ticket #502, #504, #505, #506 NO langfiles for FormOperations, InsertPagebreak, BackgroundImage, InsertPicture

---
+++ trunk/plugins/BackgroundImage/lang/no.js
@@ -0,0 +1,15 @@
+// I18N constants
+// LANG: "no", ENCODING: UTF-8
+// Author: Udo Schmal, <schmal@schaffrath-neuemedien.de>
+// translated: Kim Steinhaug, http://www.steinhaug.com/, kim@steinhaug.com
+//
+// (c) Udo Schmal & Schaffrath NeueMedien 2005
+// Distributed under the same terms as HTMLArea itself.
+// This notice MUST stay intact for use (see license.txt).
+
+{
+  "Set page background image": "Velg bakgrunnsbilde på siden",
+  "Set Page Background Image": "Velg Bakgrunnsbilde På Siden",
+  "Remove Current Background": "Fjern gjeldende bakgrunn",
+  "Cancel": "Avbryt"
+};
