321437b883611a6bf40823f7b017aeefdde2b01c
gocher <gocher@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-09-19 06:38:21 +0000

    Ticket #465 New Plugin insert Pagebreak

---
+++ trunk/plugins/InsertPagebreak/lang/de.js
@@ -0,0 +1,12 @@
+// I18N constants
+
+// LANG: "de", ENCODING: UTF-8 | ISO-8859-1
+// Author: Udo Schmal, <schmal@schaffrath-neuemedien.de>
+//
+// (c) Udo Schmal & Schaffrath NeueMedien 2004
+// Distributed under the same terms as HTMLArea itself.
+// This notice MUST stay intact for use (see license.txt).
+
+{
+	"Page break"       : "Neue Seite"
+};
