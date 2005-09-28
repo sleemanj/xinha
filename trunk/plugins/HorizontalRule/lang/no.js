40e9d1f4e86aea954fdce2d02175907f036bfd18
kimss <kimss@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-09-28 10:14:01 +0000

    NO translation for HorizontalRule in UTF-8 format.

---
+++ trunk/plugins/HorizontalRule/lang/no.js
@@ -0,0 +1,27 @@
+// I18N constants
+// LANG: "de", ENCODING: UTF-8
+// Author: Udo Schmal, <schmal@schaffrath-neuemedien.de>
+// translated: Kim Steinhaug, http://www.steinhaug.com/, kim@steinhaug.com
+//
+// (c) Udo Schmal & Schaffrath NeueMedien 2004
+// Distributed under the same terms as HTMLArea itself.
+// This notice MUST stay intact for use (see license.txt).
+
+{
+  "Insert/edit horizontal rule": "Sett inn/ rediger horisontal linje",
+  "Horizontal Rule": "Horisontal linje",
+  "Layout": "Oppsett",
+  "Width:": "Bredde:",
+  "percent": "prosent",
+  "pixels": "Piksel",
+  "Height:": "Høyde:",
+  "Alignment:": "Justering:",
+  "Left": "Venstre",
+  "Center": "Sentrert",
+  "Right": "Høyre",
+  "Style": "Stil",
+  "Color:": "Farge",
+  "No shading": "Ingen skygge",
+  "Note:": "Notat",
+  "To select an existing horizontal rule, a double-click may be needed.": "For å velge en horisontal linje kan det hende du må dobbeltklikke."
+};
