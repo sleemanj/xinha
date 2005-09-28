3c3ebba390d66cfc56d0509d48d9c4e5da81355a
gocher <gocher@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-09-28 08:20:24 +0000

    Make plugin HorizontalRule Skinable, replace image functions for buttons which don't exists, insert language support.

---
+++ trunk/plugins/HorizontalRule/lang/de.js
@@ -0,0 +1,25 @@
+// I18N constants
+// LANG: "de", ENCODING: UTF-8
+// Author: Udo Schmal, <schmal@schaffrath-neuemedien.de>
+//
+// (c) Udo Schmal & Schaffrath NeueMedien 2004
+// Distributed under the same terms as HTMLArea itself.
+// This notice MUST stay intact for use (see license.txt).
+
+{
+  "Horizontal Rule": "Horizontale Linie",
+  "Layout": "Gestaltung",
+  "Width:": "Breite:",
+	"percent": "Prozent",
+	"pixels": "Pixel",
+	"Height:": "Höhe:",
+	"Alignment:": "Ausrichtung:",
+	"Left": "links",
+	"Center": "zentriert",
+	"Right": "rechts",
+	"Style": "Stiel",
+	"Color:": "Farbe",
+	"No shading": "keine Schattierung",
+	"Note:": "Anmerkung",
+	"To select an existing horizontal rule, a double-click may be needed.": "Um eine horizontale Linie auszuwählen kann ein Doppelklick erforderlich sein."
+};
