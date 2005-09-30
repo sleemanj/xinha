3e646a5675d4046b38f792d373512ce0bd9642c9
kimss <kimss@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2005-09-30 15:02:19 +0000

    Added translation strings

--- plugins/HtmlTidy/lang/no.js
+++ plugins/HtmlTidy/lang/no.js
@@ -3,5 +3,6 @@
 // translated: Kim Steinhaug, http://www.steinhaug.com/, kim@steinhaug.com
 
 {
-  "HTML Tidy": "HTML Tidy"
+  "HTML Tidy": "HTML Tidy",
+  "Tidy failed.  Check your HTML for syntax errors.": "Tidy feilet. Sjekk HTML koden for syntax feil."
 };
