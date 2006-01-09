1fed9693fdbf536f310b316b3fa47f2e4e89bb85
mokhet <mokhet@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2006-01-09 11:34:36 +0000

    apply patch from ticket #627 (french typo correction)

--- trunk/lang/fr.js
+++ trunk/lang/fr.js
@@ -11,8 +11,8 @@
   "Justify Center": "Centrer",
   "Justify Right": "Aligner à droite",
   "Justify Full": "Justifier",
-  "Ordered List": "Numérotation",
-  "Bulleted List": "Puces",
+  "Ordered List": "Liste numérotée",
+  "Bulleted List": "Liste à puces",
   "Decrease Indent": "Diminuer le retrait",
   "Increase Indent": "Augmenter le retrait",
   "Font Color": "Couleur de police",
@@ -36,9 +36,9 @@
   "Remove formatting": "Supprimer mise en forme",
   "Select all": "Tout sélectionner",
   "Print document": "Imprimer document",
-  "Clear MSOffice tags": "Effacer tags MSOffice",
-  "Clear Inline Font Specifications": "Supprimer paramètres inline de la fonte",
-  "Split Block": "Séparer les blocks",
+  "Clear MSOffice tags": "Supprimer tags MSOffice",
+  "Clear Inline Font Specifications": "Supprimer paramètres inline de la police",
+  "Split Block": "Séparer les blocs",
   "Toggle Borders": "Afficher / Masquer les bordures",
   "Save as": "Enregistrer sous",
   "Insert/Overwrite": "Insertion / Remplacement",
@@ -52,15 +52,15 @@
   "Normal": "Normal",
   "Address": "Adresse",
   "Formatted": "Formaté",
-  
+
   //dialogs
   "OK": "OK",
   "Cancel": "Annuler",
   "Path": "Chemin",
   "You are in TEXT MODE.  Use the [<>] button to switch back to WYSIWYG.": "Vous êtes en MODE TEXTE.  Appuyez sur le bouton [<>] pour retourner au mode WYSIWYG.",
-  "The Paste button does not work in Mozilla based web browsers (technical security reasons). Press CTRL-V on your keyboard to paste directly.": "Le bouton Coller ne fonctionne pas sur les navigateurs basés sur Mozilla (pour des raisons de sécurité). Pressez simplement CTRL-V au clavier pour coller directement.",
-  "Your Document is not well formed. Check JavaScript console for details.": "Le document est mal formé. Vérifiez la console JavaScript pour plus de détail.",
-  
+  "The Paste button does not work in Mozilla based web browsers (technical security reasons). Press CTRL-V on your keyboard to paste directly.": "Le bouton Coller ne fonctionne pas sur les navigateurs basés sur Mozilla (pour des raisons de sécurité). Pressez CTRL-V au clavier pour coller directement.",
+  "Your Document is not well formed. Check JavaScript console for details.": "Le document est mal formé. Vérifiez la console JavaScript pour plus de détails.",
+
   "Alignment:": "Alignement",
   "Not set": "Indéfini",
   "Left": "Gauche",
@@ -72,16 +72,16 @@
   "Bottom": "Bas",
   "Middle": "Milieu",
   "Top": "Haut",
-  
+
   "Layout": "Mise en page",
   "Spacing": "Espacement",
   "Horizontal:": "Horizontal",
   "Horizontal padding": "Marge horizontale interne",
   "Vertical:": "Vertical",
   "Vertical padding": "Marge verticale interne",
-  "Border thickness:": "Epaisseur bordure",
+  "Border thickness:": "Epaisseur de bordure",
   "Leave empty for no border": "Laisser vide pour pas de bordure",
-  
+
   //Insert Link
   "Insert/Modify Link": "Insérer / Modifier un lien",
   "None (use implicit)": "Aucune (implicite)",
@@ -94,7 +94,7 @@
   "URL:": "URL:",
   "You must enter the URL where this link points to": "Vous devez entrer l'URL de ce lien",
   "You need to select some text before creating a link": "Vous devez sélectionner du texte avant de créer un lien",
-  
+
   // Insert Table
   "Insert Table": "Insérer un tableau",
   "Rows:": "Lignes",
@@ -113,16 +113,16 @@
   "Space between adjacent cells": "Espace entre les cellules adjacentes",
   "Cell padding:": "Marge interne",
   "Space between content and border in cell": "Espace entre le contenu et la bordure d'une cellule",
-  "You must enter a number of rows": "Vous devez entrez le nombre de lignes",
+  "You must enter a number of rows": "Vous devez entrer le nombre de lignes",
   "You must enter a number of columns": "Vous devez entrer le nombre de colonnes",
-  
+
   // Insert Image
   "Insert Image": "Insérer une image",
   "Image URL:": "URL image",
-  "Enter the image URL here": "Entrer l'url de l'image ici",
+  "Enter the image URL here": "Entrer l'URL de l'image ici",
   "Preview": "Prévisualiser",
   "Preview the image in a new window": "Prévisualiser l'image dans une nouvelle fenêtre",
-  "Alternate text:": "Text alternatif",
+  "Alternate text:": "Texte alternatif",
   "For browsers that don't support images": "Pour les navigateurs qui ne supportent pas les images",
   "Positioning of this image": "Position de l'image",
   "Image Preview:": "Prévisualisation",
