b2c02d52063f8307dae7e4bedfb5b4fcd149f22f
ray <ray@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2006-11-29 10:46:16 +0000

    Equation: new version Discussion: Ticket #877

--- plugins/Equation/lang/de.js
+++ plugins/Equation/lang/de.js
@@ -1,9 +1,21 @@
-// I18N for the Equation plugin
-// LANG: "de", ENCODING: UTF-8
-// Author: Joe Hobson, jhobson@bcoe.org
+// I18N constants
+//
+//LANG: "base", ENCODING: UTF-8
+//Author: Translator-Name, <email@example.com>
+// FOR TRANSLATORS:
+//
+//   1. PLEASE PUT YOUR CONTACT INFO IN THE ABOVE LINE
+//      (at least a valid email address)
+//
+//   2. PLEASE TRY TO USE UTF-8 FOR ENCODING;
+//      (if this is not possible, please include a comment
+//       that states what encoding is necessary.)
+
 {
-  "Equation Editor": "Gleichungs-Editor",
-  "Select operation": "Auswahl",
-  "Insert": "Einfügen",
-  "Cancel": "Abbrechen"
-};
+  "AsciiMath Formula Input": "AsciiMath Formeleditor",
+  "Formula Editor": "Formeleditor",
+  "Input":"Eingabe",
+  "Preview":"Vorschau",
+  "Based on ASCIIMathML by": "Basiert auf ASCIIMathML von",
+  "For more information on AsciiMathML visit this page:":"Für weitere Informationen besuchen Sie bitte diese Seite:"
+}
