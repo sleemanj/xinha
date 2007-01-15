68a6ddf45b1e1d38ac8fc27c43a7dd32f8a8ff76
ray <ray@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2007-01-15 23:22:56 +0000

    EFM: fixed some minor XHTML syntax stuff

--- plugins/ExtendedFileManager/manager.php
+++ plugins/ExtendedFileManager/manager.php
@@ -70,8 +70,9 @@
 		$num_rows +=2;
 	}
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<title>Insert <?php echo ($insertMode == 'image' ? 'Image' : 'File Link') ?></title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
