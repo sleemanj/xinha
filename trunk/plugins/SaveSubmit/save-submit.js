4f20ab5df2f1a768cae2cd0a0813699c099910f0
ray <ray@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2007-02-10 13:22:42 +0000

    SaveSubmit update  * improved detection of changed state  * improved handling of shortcut key  * reset changed state on udo

--- trunk/plugins/SaveSubmit/save-submit.js
+++ trunk/plugins/SaveSubmit/save-submit.js
@@ -8,18 +8,17 @@ See README.txt for information
 
 function SaveSubmit(editor) {
 	this.editor = editor;
-	this.initial_html = null;
 	this.changed = false;
 	var self = this;
 	var cfg = editor.config;
 	this.textarea = this.editor._textArea;
 
-	this.imgage_changed = _editor_url+"plugins/SaveSubmit/img/ed_save_red.gif";
-	this.imgage_unchanged = _editor_url+"plugins/SaveSubmit/img/ed_save_green.gif";
+	this.image_changed = _editor_url+"plugins/SaveSubmit/img/ed_save_red.gif";
+	this.image_unchanged = _editor_url+"plugins/SaveSubmit/img/ed_save_green.gif";
 	cfg.registerButton({
 	id       : "savesubmit",
 	tooltip  : self._lc("Save"),
-	image    : this.imgage_unchanged,
+	image    : this.image_unchanged,
 	textMode : false,
 	action   :  function(editor) {
 			self.save(editor);
@@ -43,58 +42,54 @@ SaveSubmit._pluginInfo = {
   license       : "htmlArea"
 }
 
-SaveSubmit.prototype.onGenerate = function() {
-	var self = this;
-	var doc = this.editordoc = this.editor._iframe.contentWindow.document;
-	Xinha._addEvents(doc, ["mouseup","keyup","keypress","keydown"],
-			    function (event) {
-			    return self.onEvent(Xinha.is_ie ? self.editor._iframe.contentWindow.event : event);
-			    });
+SaveSubmit.prototype.onGenerateOnce = function() {
+	this.initial_html = this.editor.getInnerHTML();
 }
 
-SaveSubmit.prototype.onEvent = function(ev) {
-
-	var keyEvent = (Xinha.is_ie && ev.type == "keydown") || (!Xinha.is_ie && ev.type == "keypress");
-
-	if (keyEvent && ev.ctrlKey && String.fromCharCode(Xinha.is_ie ? ev.keyCode : ev.charCode).toLowerCase() == 's') {
+SaveSubmit.prototype.onKeyPress = function(ev) {
+	if ( ev.ctrlKey && this.editor.getKey(ev) == 's') {
 			this.save(this.editor);
+			Xinha._stopEvent(ev);
+			return true;
 	}
 	else {
 		if (!this.changed) {
 			if (this.getChanged()) this.setChanged();
+			return false;
 		}
 	}
 }
-
+SaveSubmit.prototype.onExecCommand = function (cmd) {
+	if (this.changed && cmd == 'undo') { 
+		if (this.initial_html == this.editor.getInnerHTML()) this.setUnChanged();
+		return false;
+	}
+}
+SaveSubmit.prototype.onUpdateToolbar = function () {
+	if (!this.changed) {
+		if (this.getChanged()) this.setChanged();
+		return false;
+	}	
+}
 SaveSubmit.prototype.getChanged = function() {
-	if (this.initial_html == null) this.initial_html = this.editor.getHTML();
-	if (this.initial_html != this.editor.getHTML() && this.changed == false) {
-
+	if (this.initial_html === null) this.initial_html = this.editor.getInnerHTML();
+	if (this.initial_html != this.editor.getInnerHTML() && this.changed == false) {
 		this.changed = true;
 		return true;
 	}
 	else return false;
 }
 SaveSubmit.prototype.setChanged = function() {
-	toolbar_objects = this.editor._toolbarObjects;
-	for (var i in toolbar_objects) {
-		var btn = toolbar_objects[i];
-		if (btn.name == 'savesubmit') {
-			btn.swapImage(this.imgage_changed);
-		}
-	}
+	this.editor._toolbarObjects.savesubmit.swapImage(this.image_changed);
 	this.editor.updateToolbar();
 }
-SaveSubmit.prototype.changedReset = function() {
+SaveSubmit.prototype.setUnChanged = function() {
 	this.changed = false;
+	this.editor._toolbarObjects.savesubmit.swapImage(this.image_unchanged);
+}
+SaveSubmit.prototype.changedReset = function() {
 	this.initial_html = null;
-	toolbar_objects = this.editor._toolbarObjects;
-	for (var i in toolbar_objects) {
-		var btn = toolbar_objects[i];
-		if (btn.name == 'savesubmit') {
-			btn.swapImage(this.imgage_unchanged);
-		}
-	}
+	this.setUnChanged();
 }
 
 SaveSubmit.prototype.save =  function(editor) {
@@ -119,21 +114,20 @@ SaveSubmit.prototype.save =  function(editor) {
 		}
 		removeMessage = function() { self.removeMessage()} ;
 		window.setTimeout("removeMessage()",1000);
-
 	});
 }
 
 SaveSubmit.prototype.setMessage = function(string) {
   var textarea = this.textarea;
-  if ( !document.getElementById("message_sub_" + textarea.name)) { return ; }
-  var elt = document.getElementById("message_sub_" + textarea.name);
+  if ( !document.getElementById("message_sub_" + textarea.id)) { return ; }
+  var elt = document.getElementById("message_sub_" + textarea.id);
   elt.innerHTML = Xinha._lc(string, 'SaveSubmit');
 }
 
 SaveSubmit.prototype.removeMessage = function() {
   var textarea = this.textarea;
-  if (!document.getElementById("message_" + textarea.name)) { return ; }
-  document.body.removeChild(document.getElementById("message_" + textarea.name));
+  if (!document.getElementById("message_" + textarea.id)) { return ; }
+  document.body.removeChild(document.getElementById("message_" + textarea.id));
 }
 
 //ripped mokhet's loading message function
@@ -142,7 +136,7 @@ SaveSubmit.prototype.buildMessage   = function() {
 	var textarea = this.textarea;
 	var htmlarea = this.editor._htmlArea;
 	var loading_message = document.createElement("div");
-	loading_message.id = "message_" + textarea.name;
+	loading_message.id = "message_" + textarea.id;
 	loading_message.className = "loading";
 	loading_message.style.width    = htmlarea.offsetWidth +'px' ;//(this.editor._iframe.offsetWidth != 0) ? this.editor._iframe.offsetWidth +'px' : this.editor._initial_ta_size.w;
 
@@ -151,12 +145,12 @@ SaveSubmit.prototype.buildMessage   = function() {
 
 	var loading_main = document.createElement("div");
 	loading_main.className = "loading_main";
-	loading_main.id = "loading_main_" + textarea.name;
+	loading_main.id = "loading_main_" + textarea.id;
 	loading_main.appendChild(document.createTextNode(this._lc("Saving...")));
 
 	var loading_sub = document.createElement("div");
 	loading_sub.className = "loading_sub";
-	loading_sub.id = "message_sub_" + textarea.name;
+	loading_sub.id = "message_sub_" + textarea.id;
 	loading_sub.appendChild(document.createTextNode(this._lc("in progress")));
 	loading_message.appendChild(loading_main);
 	loading_message.appendChild(loading_sub);
