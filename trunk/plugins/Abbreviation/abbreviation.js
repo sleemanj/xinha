5a4499b95487d3c6259c2cd4375c7e812f5628f3
gocher <gocher@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2007-06-03 12:39:44 +0000

    change htmlarea to Xinha in plugin Abbreviation

--- trunk/plugins/Abbreviation/abbreviation.js
+++ trunk/plugins/Abbreviation/abbreviation.js
@@ -1,4 +1,4 @@
-// Abbreviation plugin for HTMLArea
+// Abbreviation plugin for Xinha
 // Implementation by Udo Schmal & Schaffrath NeueMedien
 // Original Author - Udo Schmal
 //
@@ -7,36 +7,36 @@
 // This notice MUST stay intact for use (see license.txt).
 
 function Abbreviation(editor) {
-	this.editor = editor;
-	var cfg = editor.config;
-	var self = this;
+  this.editor = editor;
+  var cfg = editor.config;
+  var self = this;
 
-	// register the toolbar buttons provided by this plugin
-	cfg.registerButton({
-	id       : "abbreviation",
-	tooltip  : this._lc("Abbreviation"),
-	image    : editor.imgURL("ed_abbreviation.gif", "Abbreviation"),
-	textMode : false,
-	action   : function(editor) {
-			self.buttonPress(editor);
-		}
-	})
-	cfg.addToolbarElement("abbreviation", "inserthorizontalrule", 1);
+  // register the toolbar buttons provided by this plugin
+  cfg.registerButton({
+    id       : "abbreviation",
+    tooltip  : this._lc("Abbreviation"),
+    image    : editor.imgURL("ed_abbreviation.gif", "Abbreviation"),
+    textMode : false,
+    action   : function(editor) {
+                 self.buttonPress(editor);
+               }
+  })
+  cfg.addToolbarElement("abbreviation", "inserthorizontalrule", 1);
 }
 
 Abbreviation._pluginInfo = {
-	name          : "Abbreviation",
-	version       : "1.0",
-	developer     : "Udo Schmal",
-	developer_url : "",
-	sponsor       : "L.N.Schaffrath NeueMedien",
-	sponsor_url   : "http://www.schaffrath-neuemedien.de/",
-	c_owner       : "Udo Schmal & Schaffrath-NeueMedien",
-	license       : "htmlArea"
+  name          : "Abbreviation",
+  version       : "1.0",
+  developer     : "Udo Schmal",
+  developer_url : "",
+  sponsor       : "L.N.Schaffrath NeueMedien",
+  sponsor_url   : "http://www.schaffrath-neuemedien.de/",
+  c_owner       : "Udo Schmal & Schaffrath-NeueMedien",
+  license       : "htmlArea"
 };
 
 Abbreviation.prototype._lc = function(string) {
-    return HTMLArea._lc(string, 'Abbreviation');
+    return Xinha._lc(string, 'Abbreviation');
 };
 
 Abbreviation.prototype.onGenerate = function() {
@@ -52,24 +52,24 @@ Abbreviation.prototype.onGenerate = function() {
 };
 
 Abbreviation.prototype.buttonPress = function(editor, context, updatecontextclass) {
-	var outparam = null;
-	var html = editor.getSelectedHTML();
-	var sel  = editor._getSelection();
-	var range  = editor._createRange(sel);
-	var abbr = editor._activeElement(sel);
-	  if(!(abbr != null && abbr.tagName.toLowerCase() == "abbr")) {
-	    abbr = editor._getFirstAncestor(sel, 'abbr'); 
+  var outparam = null;
+  var html = editor.getSelectedHTML();
+  var sel  = editor._getSelection();
+  var range  = editor._createRange(sel);
+  var abbr = editor._activeElement(sel);
+  if(!(abbr != null && abbr.tagName.toLowerCase() == "abbr")) {
+    abbr = editor._getFirstAncestor(sel, 'abbr');
   }
   if (abbr != null && abbr.tagName.toLowerCase() == "abbr")
     outparam = { title : abbr.title,
                  text : abbr.innerHTML};
-	else	
-	  outparam = { title : '',
-	               text : html};
-	  
-	editor._popupDialog( "plugin://Abbreviation/abbreviation", function( param )	{
-		if ( param ) {
-			var title = param["title"];
+  else
+    outparam = { title : '',
+                 text : html};
+
+  editor._popupDialog( "plugin://Abbreviation/abbreviation", function( param ) {
+    if ( param ) {
+      var title = param["title"];
       if (title == "" || title == null) {
         if (abbr) {
           var child = abbr.innerHTML;
@@ -77,14 +77,14 @@ Abbreviation.prototype.buttonPress = function(editor, context, updatecontextclas
           editor.insertHTML(child);
         }
         return;
-      } 
+      }
       try {
         var doc = editor._doc;
         if (!abbr) {
           abbr = doc.createElement("abbr");
           abbr.title = title;
           abbr.innerHTML = html;
-          if (HTMLArea.is_ie) {
+          if (Xinha.is_ie) {
             range.pasteHTML(abbr.outerHTML);
           } else {
             editor.insertNodeAtSelection(abbr);
