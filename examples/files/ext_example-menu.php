e1c72d92f3a2d79f421cfefb5a2ebefbfe99a64c
ray <ray@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2008-04-12 21:23:17 +0000

    small fixes to the extended example menu

--- examples/files/ext_example-menu.php
+++ examples/files/ext_example-menu.php
@@ -116,7 +116,7 @@ function Dialog(url, action, init) {
 	if (typeof window.showModalDialog == 'function')
 	{
 		Dialog._return = action;
-		var r = window.showModalDialog(url, init, "dialogheight=10;dialogheight=10;scroll=no;resizable=yes");
+		var r = window.showModalDialog(url, init, "dialogheight=10;dialogheight=10;scroll=yes;resizable=yes");
 	}
 	else
 	{
