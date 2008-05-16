e8820f7d98b2d37c08e92c6b6733992ae26d5983
douglas <douglas@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2008-05-16 18:04:41 +0000

    Ticket #1213 partial rollback of duplicated code...

--- plugins/UnsavedChanges/lang/fr_ca.js
+++ plugins/UnsavedChanges/lang/fr_ca.js
@@ -3,8 +3,3 @@
 {
   "You have unsaved changes in the editor": "Tu n'as pas enregistré tes modifications"
 };
-// I18N constants
-// LANG: "fr_ca", ENCODING: UTF-8
-{
-  "You have unsaved changes in the editor": "Tu n'as pas enregistré tes modifications"
-};
