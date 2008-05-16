763a31027c34e08cb77234d1eaf09624f45c374c
douglas <douglas@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2008-05-16 17:57:55 +0000

    Ticket #1213 Enhancement to prevent users from accidentally throwing away their changes...

---
+++ plugins/UnsavedChanges/lang/fr.js
@@ -0,0 +1,10 @@
+// I18N constants
+// LANG: "fr", ENCODING: UTF-8
+{
+  "You have unsaved changes in the editor": "Vous n'avez pas enregistré vos modifications"
+};
+// I18N constants
+// LANG: "fr", ENCODING: UTF-8
+{
+  "You have unsaved changes in the editor": "Vous n'avez pas enregistré vos modifications"
+};
