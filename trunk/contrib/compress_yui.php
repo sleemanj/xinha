9bb52600ca2a28c210ea59746e6387031cf718ee
ray <ray@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2009-03-20 13:07:59 +0000

    updated compression script

--- trunk/contrib/compress_yui.php
+++ trunk/contrib/compress_yui.php
@@ -1,7 +1,13 @@
 <?
 // This is script that uses the YUI compressor (http://www.julienlecomte.net/blog/2007/08/11/)
 // It yields gradually better results than the dojo comressor, but it produces unreadable code
-die("Run this script to batch-compress the current Xinha snapshot. To run the script, open the file and comment out the die() command");
+//die("Run this script to batch-compress the current Xinha snapshot. To run the script, open the file and comment out the die() command");
+
+$repository_url = 'http://svn.xinha.org/trunk';
+$version ='';
+$date = date('r');
+
+$xinha_root = realpath(dirname(__FILE__).'/..');
 
 error_reporting(E_ALL);
 ini_set('show_errors',1);
@@ -47,11 +53,13 @@ function scan($dir, $durl = '',$min_size="3000")
 
 	return $files;
 }
-scan("../",0);
-$cwd = getcwd();
+
+scan($xinha_root,0);
+
 print "Processing ".count($return)." files<br />";
 
-$prefix = "/* This compressed file is part of Xinha. For uncomressed sources, forum, and bug reports, go to xinha.org */";
+$prefix = "/* This compressed file is part of Xinha. For uncompressed sources, forum, and bug reports, go to xinha.org */";
+if ($version) $prefix .= "\n/* This file is part of version $version released $date */";
 $core_prefix = '
   /*--------------------------------------------------------------------------
     --  Xinha (is not htmlArea) - http://xinha.org
@@ -59,7 +67,7 @@ $core_prefix = '
     --  Use of Xinha is granted by the terms of the htmlArea License (based on
     --  BSD license)  please read license.txt in this package for details.
     --
-    --  Copyright (c) 2005-2009 Xinha Developer Team and contributors
+    --  Copyright (c) 2005-'.date('Y').' Xinha Developer Team and contributors
     --  
     --  Xinha was originally based on work by Mihai Bazon which is:
     --      Copyright (c) 2003-2004 dynarch.com.
@@ -71,13 +79,19 @@ $core_prefix = '
 foreach ($return as $file)
 {
 	set_time_limit ( 60 ); 
-	print "Processing $file<br />";
+	print "Processing $file\n";
 	flush();
+	
+	$file_url = $repository_url.str_replace($xinha_root,'',$file);
+
+	copy($file,$file."_uncompr.js");
+	
+	$file_prefix = $prefix."\n/* The URL of the most recent uncompressed version of this file is $file_url */";
 	$ext = preg_replace('/.*?(\.js|\.css)$/','$1',$file);
 	
 	file_put_contents($file."_uncompr${ext}", preg_replace('/(\/\/[^\n]*)?(?![*])\\\[\n]/','',file_get_contents($file)));
 
-	passthru("echo \"".(preg_match('/XinhaCore.js$/',$file) ? $prefix.$core_prefix : $prefix)."\" > $file && java -jar ${cwd}/yuicompressor-2.4.2.jar  --charset UTF-8 ${file}_uncompr${ext} >> $file 2>&1");
+	passthru("echo \"".(preg_match('/XinhaCore.js$/',$file) ? $file_prefix.$core_prefix : $prefix)."\" > $file && java -jar {$xinha_root}/contrib/yuicompressor-2.4.2.jar  --charset UTF-8 ${file}_uncompr${ext} >> $file 2>&1");
 	if (preg_match('/\d+:\d+:syntax error/',file_get_contents($file)))
 	{
 		unlink($file);
