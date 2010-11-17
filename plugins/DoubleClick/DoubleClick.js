b54683dad78aaf817c5db788ae1e5adddd5d7072
ejucovy <ejucovy@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2010-11-17 17:27:20 +0000

    refs #1555 (applying patch http://trac.xinha.org/attachment/ticket/1555/dblclickList.diff) -- strip out functionality from the DoubleClick plugin, because the functionality now exists in core as the config.dblclickList option.  so, if this plugin is loaded, it will just copy its config into the now-built-in config.  core will handle the rest.

--- plugins/DoubleClick/DoubleClick.js
+++ plugins/DoubleClick/DoubleClick.js
@@ -15,7 +15,7 @@
 
 DoubleClick._pluginInfo = {
   name          : "DoubleClick",
-  version       : "1.0",
+  version       : "1.1",
   developer     : "Marijn Kampf",
   developer_url : "http://www.marijn.org",
   c_owner       : "Marijn Kampf",
@@ -48,19 +48,15 @@ function DoubleClick(editor) {
 
 DoubleClick.prototype.onGenerate = function() {
   var self = this;
-  var doc = this.editordoc = this.editor._iframe.contentWindow.document;
-  Xinha._addEvents(doc, ["dblclick"],
-          function (event) {
-          return self.onDoubleClick(Xinha.is_ie ? self.editor._iframe.contentWindow.event : event);
-          });
-  this.currentClick = null;
-};
-
-DoubleClick.prototype.onDoubleClick = function(ev) {
-  var target = Xinha.is_ie ? ev.srcElement : ev.target;
-  var tagName = target.tagName.toLowerCase();
-
-  if (this.editor.dblClickList[tagName] != undefined) {
-    this.editor.dblClickList[tagName][0](this.editor, target);
+  var config = this.editor.config;
+  for( var i in this.editor.dblClickList ) {
+      if( typeof i != 'string' ) {
+	  continue;
+      }
+      var actions = this.editor.dblClickList[i];
+      if( typeof actions != 'object' ) {
+	  continue;
+      }
+      this.editor.config.dblclickList[i] = actions;
   }
 };
