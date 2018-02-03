58200ad0413fc9e94b8010db748836457c6061a6
James Sleeman <james@gogo.co.nz>
2018-02-03 12:25:08 +0000

    #1584 Turkish Translations

---
+++ plugins/InsertSmiley/lang/tr.js
@@ -0,0 +1,12 @@
+// I18N constants
+// LANG: "tr", ENCODING: UTF-8
+// Sponsored by http://www.km.co.at/
+// Author: Serdar Domurcuk, <serdar.domurcuk@km.co.at>
+// (c) Knowledge Markets 2011
+// Distributed under the same terms as HTMLArea itself.
+// This notice MUST stay intact for use (see license.txt).
+{ 
+	"Insert Smiley": "Gülen yüz ekle",
+	"Smiley": "Gülen yüz",
+	"Cancel": "İptal"
+};
