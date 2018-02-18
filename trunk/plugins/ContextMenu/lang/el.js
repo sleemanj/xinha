bb6e60e087eb35ee84660283e8c96b18d97662e7
gogo <gogo@bdcdd039-d5ef-0310-875c-f7949af66bfe>
2018-02-18 22:36:38 +0000

    Repair Greek translations which must have been corrupted some time (character set conversion issue).

--- trunk/plugins/ContextMenu/lang/el.js
+++ trunk/plugins/ContextMenu/lang/el.js
@@ -2,46 +2,53 @@
 // LANG: "el", ENCODING: UTF-8
 // Author: Dimitris Glezos, dimitris@glezos.com
 {
-  "Cut": "ΞΟΞΏΞΊΞΏΟΞ�",
-  "Copy": "ΞΞ½ΟΞΉΞ³ΟΞ±ΟΞ�",
-  "Paste": "ΞΟΞΉΞΊΟΞ»Ξ»Ξ·ΟΞ·",
-  "_Image Properties...": "ΞΞ΄ΞΉΟΟΞ·ΟΞ΅Ο ΞΞΉΞΊΟΞ½Ξ±Ο...",
-  "_Modify Link...": "Ξ�ΟΞΏΟΞΏΟΞΏΞ―Ξ·ΟΞ· ΟΟΞ½Ξ΄Ξ­ΟΞΌΞΏΟ...",
-  "Chec_k Link...": "ΞΞ»Ξ΅Ξ³ΟΞΏΟ ΟΟΞ½Ξ΄Ξ­ΟΞΌΟΞ½...",
-  "_Remove Link...": "ΞΞΉΞ±Ξ³ΟΞ±ΟΞ� ΟΟΞ½Ξ΄Ξ­ΟΞΌΞΏΟ...",
-  "C_ell Properties...": "ΞΞ΄ΞΉΟΟΞ·ΟΞ΅Ο ΞΊΞ΅Ξ»ΞΉΞΏΟ...",
-  "Ro_w Properties...": "ΞΞ΄ΞΉΟΟΞ·ΟΞ΅Ο Ξ³ΟΞ±ΞΌΞΌΞ�Ο...",
-  "I_nsert Row Before": "ΞΞΉΟΞ±Ξ³ΟΞ³Ξ� Ξ³ΟΞ±ΞΌΞΌΞ�Ο ΟΟΞΉΞ½",
-  "In_sert Row After": "ΞΞΉΟΞ±Ξ³ΟΞ³Ξ� Ξ³ΟΞ±ΞΌΞΌΞ�Ο ΞΌΞ΅ΟΞ¬",
-  "_Delete Row": "ΞΞΉΞ±Ξ³ΟΞ±ΟΞ� Ξ³ΟΞ±ΞΌΞΌΞ�Ο",
-  "_Table Properties...": "ΞΞ΄ΞΉΟΟΞ·ΟΞ΅Ο ΟΞ―Ξ½Ξ±ΞΊΞ±...",
-  "Insert _Column Before": "ΞΞΉΟΞ±Ξ³ΟΞ³Ξ� ΟΟΞ�Ξ»Ξ·Ο ΟΟΞΉΞ½",
-  "Insert C_olumn After": "ΞΞΉΟΞ±Ξ³ΟΞ³Ξ� ΟΟΞ�Ξ»Ξ·Ο ΞΌΞ΅ΟΞ¬",
-  "De_lete Column": "ΞΞΉΞ±Ξ³ΟΞ±ΟΞ� ΟΟΞ�Ξ»Ξ·Ο",
-  "Justify Left": "Ξ£ΟΞΏΞ―ΟΞ·ΟΞ· ΞΟΞΉΟΟΞ΅ΟΞ¬",
-  "Justify Center": "Ξ£ΟΞΏΞ―ΟΞ·ΟΞ· ΞΞ­Ξ½ΟΟΞΏ",
-  "Justify Right": "Ξ£ΟΞΏΞ―ΟΞ·ΟΞ· ΞΞ΅ΞΎΞΉΞ¬",
-  "Justify Full": "Ξ Ξ»Ξ�ΟΞ·Ο Ξ£ΟΞΏΞ―ΟΞ·ΟΞ·",
-  "Make lin_k...": "ΞΞ·ΞΌΞΉΞΏΟΟΞ³Ξ―Ξ± ΟΟΞ½Ξ΄Ξ­ΟΞΌΞΏΟ...",
-  "Remove the $elem Element...": "ΞΟΞ±Ξ―ΟΞ΅ΟΞ· $elem ΟΟΞΏΞΉΟΞ΅Ξ―ΞΏΟ...",
-  "Please confirm that you want to remove this element:": "ΞΞ―ΟΟΞ΅ Ξ²Ξ­Ξ²Ξ±ΞΉΞΏΟ ΟΟΟ ΞΈΞ­Ξ»Ξ΅ΟΞ΅ Ξ½Ξ± Ξ±ΟΞ±ΞΉΟΞ­ΟΞ΅ΟΞ΅ ΟΞΏ ΟΟΞΏΞΉΟΞ΅Ξ―ΞΏ ",
-  "Remove this node from the document": "ΞΟΞ±Ξ―ΟΞ΅ΟΞ· Ξ±ΟΟΞΏΟ ΟΞΏΟ ΞΊΟΞΌΞ²ΞΏΟ Ξ±ΟΟ ΟΞΏ Ξ­Ξ³Ξ³ΟΞ±ΟΞΏ",
-  "How did you get here? (Please report!)": "Ξ ΟΟ Ξ�ΟΞΈΞ±ΟΞ΅ ΞΌΞ­ΟΟΞΉ Ξ΅Ξ΄Ο; (Ξ Ξ±ΟΞ±ΞΊΞ±Ξ»ΞΏΟΞΌΞ΅ Ξ±Ξ½Ξ±ΟΞ­ΟΞ΅ΟΞ΅ ΟΞΏ!)",
-  "Show the image properties dialog": "ΞΞΌΟΞ¬Ξ½ΞΉΟΞ· Ξ΄ΞΉΞ±Ξ»ΟΞ³ΞΏΟ ΞΌΞ΅ ΟΞΉΟ ΞΞ΄ΞΉΟΟΞ·ΟΞ΅Ο Ξ΅ΞΉΞΊΟΞ½Ξ±Ο",
-  "Modify URL": "Ξ�ΟΞΏΟΞΏΟΞΏΞ―Ξ·ΟΞ· URL",
-  "Current URL is": "Ξ�ΞΏ ΟΟΞ­ΟΟΞ½ URL Ξ΅Ξ―Ξ½Ξ±ΞΉ",
-  "Opens this link in a new window": "ΞΞ½ΞΏΞ―Ξ³Ξ΅ΞΉ Ξ±ΟΟΟ ΟΞΏΞ½ ΟΟΞ½Ξ΄Ξ΅ΟΞΌΞΏ ΟΞ΅ Ξ­Ξ½Ξ± Ξ½Ξ­ΞΏ ΟΞ±ΟΞ¬ΞΈΟΟΞΏ",
-  "Please confirm that you want to unlink this element.": "ΞΞ―ΟΟΞ΅ Ξ²Ξ­Ξ²Ξ±ΞΉΞΏΟ ΟΟΟ ΞΈΞ­Ξ»Ξ΅ΟΞ΅ Ξ½Ξ± Ξ±ΟΞ±ΞΉΟΞ­ΟΞ΅ΟΞ΅ ΟΞΏΞ½ ΟΟΞ½Ξ΄Ξ΅ΟΞΌΞΏ Ξ±ΟΟ Ξ±ΟΟΟ ΟΞΏ ΟΟΞΏΞΉΟΞ΅Ξ―ΞΏ:",
-  "Link points to:": "Ξ ΟΟΞ½Ξ΄Ξ΅ΞΌΞΏΟ ΞΏΞ΄Ξ·Ξ³Ξ΅Ξ― Ξ΅Ξ΄Ο:",
-  "Unlink the current element": "ΞΟΞ±Ξ―ΟΞ΅ΟΞ· ΟΟΞ½Ξ΄Ξ­ΟΞΌΞΏΟ Ξ±ΟΟ ΟΞΏ ΟΞ±ΟΟΞ½ ΟΟΞΏΞΉΟΞ΅Ξ―ΞΏ",
-  "Show the Table Cell Properties dialog": "ΞΞΌΟΞ¬Ξ½ΞΉΟΞ· Ξ΄ΞΉΞ±Ξ»ΟΞ³ΞΏΟ ΞΌΞ΅ ΟΞΉΟ ΞΞ΄ΞΉΟΟΞ·ΟΞ΅Ο ΞΊΞ΅Ξ»ΞΉΞΏΟ Ξ Ξ―Ξ½Ξ±ΞΊΞ±",
-  "Show the Table Row Properties dialog": "ΞΞΌΟΞ¬Ξ½ΞΉΟΞ· Ξ΄ΞΉΞ±Ξ»ΟΞ³ΞΏΟ ΞΌΞ΅ ΟΞΉΟ ΞΞ΄ΞΉΟΟΞ·ΟΞ΅Ο Ξ³ΟΞ±ΞΌΞΌΞ�Ο Ξ Ξ―Ξ½Ξ±ΞΊΞ±",
-  "Insert a new row before the current one": "ΞΞΉΟΞ±Ξ³ΟΞ³Ξ� ΞΌΞΉΞ±Ο Ξ½Ξ­Ξ±Ο Ξ³ΟΞ±ΞΌΞΌΞ�Ο ΟΟΞΉΞ½ ΟΞ·Ξ½ Ξ΅ΟΞΉΞ»Ξ΅Ξ³ΞΌΞ­Ξ½Ξ·",
-  "Insert a new row after the current one": "ΞΞΉΟΞ±Ξ³ΟΞ³Ξ� ΞΌΞΉΞ±Ο Ξ½Ξ­Ξ±Ο Ξ³ΟΞ±ΞΌΞΌΞ�Ο ΞΌΞ΅ΟΞ¬ ΟΞ·Ξ½ Ξ΅ΟΞΉΞ»Ξ΅Ξ³ΞΌΞ­Ξ½Ξ·",
-  "Delete the current row": "ΞΞΉΞ±Ξ³ΟΞ±ΟΞ� Ξ΅ΟΞΉΞ»Ξ΅Ξ³ΞΌΞ­Ξ½Ξ·Ο Ξ³ΟΞ±ΞΌΞΌΞ�Ο",
-  "Show the Table Properties dialog": "ΞΞΌΟΞ¬Ξ½ΞΉΟΞ· Ξ΄ΞΉΞ±Ξ»ΟΞ³ΞΏΟ ΞΌΞ΅ ΟΞΉΟ ΞΞ΄ΞΉΟΟΞ·ΟΞ΅Ο Ξ Ξ―Ξ½Ξ±ΞΊΞ±",
-  "Insert a new column before the current one": "ΞΞΉΟΞ±Ξ³ΟΞ³Ξ� Ξ½Ξ­Ξ±Ο ΟΟΞ�Ξ»Ξ·Ο ΟΟΞΉΞ½ ΟΞ·Ξ½ Ξ΅ΟΞΉΞ»Ξ΅Ξ³ΞΌΞ­Ξ½Ξ·",
-  "Insert a new column after the current one": "ΞΞΉΟΞ±Ξ³ΟΞ³Ξ� Ξ½Ξ­Ξ±Ο ΟΟΞ�Ξ»Ξ·Ο ΞΌΞ΅ΟΞ¬ ΟΞ·Ξ½ Ξ΅ΟΞΉΞ»Ξ΅Ξ³ΞΌΞ­Ξ½Ξ·",
-  "Delete the current column": "ΞΞΉΞ±Ξ³ΟΞ±ΟΞ� Ξ΅ΟΞΉΞ»Ξ΅Ξ³ΞΌΞ­Ξ½Ξ·Ο ΟΟΞ�Ξ»Ξ·Ο",
-  "Create a link": "ΞΞ·ΞΌΞΉΞΏΟΟΞ³Ξ―Ξ± ΟΟΞ½Ξ΄Ξ­ΟΞΌΞΏΟ"
+    "C_ell Properties...": "Ιδιότητες κελιού...",
+    "Chec_k Link...": "Έλεγχος συνδέσμων...",
+    "Copy": "Αντιγραφή",
+    "Create a link": "Δημιουργία συνδέσμου",
+    "Current URL is": "Το τρέχων URL είναι",
+    "Cut": "Αποκοπή",
+    "De_lete Column": "Διαγραφή στήλης",
+    "Delete Cell": "",
+    "Delete the current column": "Διαγραφή επιλεγμένης στήλης",
+    "Delete the current row": "Διαγραφή επιλεγμένης γραμμής",
+    "How did you get here? (Please report!)": "Πώς ήρθατε μέχρι εδώ; (Παρακαλούμε αναφέρετε το!)",
+    "I_nsert Row Before": "Εισαγωγή γραμμής πριν",
+    "In_sert Row After": "Εισαγωγή γραμμής μετά",
+    "Insert C_olumn After": "Εισαγωγή στήλης μετά",
+    "Insert Cell After": "",
+    "Insert Cell Before": "",
+    "Insert _Column Before": "Εισαγωγή στήλης πριν",
+    "Insert a new column after the current one": "Εισαγωγή νέας στήλης μετά την επιλεγμένη",
+    "Insert a new column before the current one": "Εισαγωγή νέας στήλης πριν την επιλεγμένη",
+    "Insert a new row after the current one": "Εισαγωγή μιας νέας γραμμής μετά την επιλεγμένη",
+    "Insert a new row before the current one": "Εισαγωγή μιας νέας γραμμής πριν την επιλεγμένη",
+    "Insert a paragraph after the current node": "",
+    "Insert a paragraph before the current node": "",
+    "Insert paragraph after": "",
+    "Insert paragraph before": "",
+  "Justify Left"                                          : "Στοίχηση Αριστερά",
+  "Justify Center"                                        : "Στοίχηση Κέντρο",
+  "Justify Right"                                         : "Στοίχηση Δεξιά",
+  "Justify Full"                                          : "Πλήρης Στοίχηση",
+    "Link points to:": "Το τρέχων URL είναι",
+    "Make lin_k...": "Δημιουργία συνδέσμου...",
+    "Merge Cells": "",
+    "Opens this link in a new window": "Ανοίγει αυτό τον σύνδεσμο σε ένα νέο παράθυρο",
+    "Paste": "Επικόλληση",
+    "Please confirm that you want to remove this element:": "",
+    "Please confirm that you want to unlink this element.": "Είστε βέβαιος πως θέλετε να αφαιρέσετε τον σύνδεσμο από αυτό το στοιχείο:",
+    "Remove the $elem Element...": "",
+    "Remove this node from the document": "",
+    "Ro_w Properties...": "Ιδιότητες γραμμής...",
+    "Show the Table Cell Properties dialog": "Ιδιότητες κελιού...",
+    "Show the Table Properties dialog": "Εμφάνιση διαλόγου με τις Ιδιότητες Πίνακα",
+    "Show the Table Row Properties dialog": "Ιδιότητες γραμμής...",
+    "Show the image properties dialog": "Ιδιότητες Εικόνας...",
+    "Unlink the current element": "Αφαίρεση συνδέσμου από το παρών στοιχείο",
+    "_Delete Row": "Διαγραφή γραμμής",
+    "_Image Properties...": "Ιδιότητες Εικόνας...",
+    "_Modify Link...": "Τροποποίηση συνδέσμου...",
+    "_Remove Link...": "Διαγραφή συνδέσμου...",
+    "_Table Properties...": "Ιδιότητες πίνακα..."
 };
